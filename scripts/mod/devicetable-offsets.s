5773342fd0f79058b47543daeae76a5a3b5aee53
derfelot <5124143+derfelot@users.noreply.github.com>
2020-02-25 23:02:47 +0100

    G970FXXU3ASIG
    SM-G970F_PP_Opensource (G970FXXU3ASIG)
    
    source: https://opensource.samsung.com
    
    Changes made:
    
    find . -type f -name '.gitignore' -delete
    find . -type f -name 'modules.builtin' -delete
    find . -type f -name 'modules.order' -delete
    find . -type f -print0 | xargs -0 dos2unix
    find . -type f -iname "*.sh" -exec chmod a+x {} \;
    find . -type f -iname "*.py" -exec chmod a+x {} \;
    rm -rf out/

--- scripts/mod/devicetable-offsets.s
+++ scripts/mod/devicetable-offsets.s
@@ -3,8 +3,7 @@
 	.globl	main                    // -- Begin function main
 	.p2align	2
 	.type	main,@function
-	nop                             // @main
-main:
+main:                                   // @main
 .Lfunc_begin0:
 	.file	1 "scripts/mod/devicetable-offsets.c"
 	.loc	1 10 0                  // scripts/mod/devicetable-offsets.c:10:0
@@ -821,15 +820,15 @@ main:
                                         // -- End function
 	.section	.debug_str,"MS",@progbits,1
 .Linfo_string0:
-	.asciz	"Android (4639204-cfp-jopp based on r316199) clang version 6.0.1 (https://android.googlesource.com/toolchain/clang 279c0d3a962121a6d1d535e7b0b5d9d36d3c829d) (https://android.googlesource.com/toolchain/llvm aadd87ffb6a2eafcb577913073d46b20195a9cdc) (based on LLVM 6.0.1svn)" // string offset=0
+	.asciz	"Android (4639204 based on r316199) clang version 6.0.1 (https://android.googlesource.com/toolchain/clang 279c0d3a962121a6d1d535e7b0b5d9d36d3c829d) (https://android.googlesource.com/toolchain/llvm aadd87ffb6a2eafcb577913073d46b20195a9cdc) (based on LLVM 6.0.1svn)" // string offset=0
 .Linfo_string1:
-	.asciz	"scripts/mod/devicetable-offsets.c" // string offset=272
+	.asciz	"scripts/mod/devicetable-offsets.c" // string offset=263
 .Linfo_string2:
-	.asciz	"/home/dpi/qb5_8814/workspace/P4_1716/android/kernel/exynos9820" // string offset=306
+	.asciz	"/home/thuy/GATHER/G970F/OPENSOURCE_G970FXXU3ASIG_CL16887086_Kernel" // string offset=297
 .Linfo_string3:
-	.asciz	"main"                  // string offset=369
+	.asciz	"main"                  // string offset=364
 .Linfo_string4:
-	.asciz	"int"                   // string offset=374
+	.asciz	"int"                   // string offset=369
 	.section	.debug_abbrev,"",@progbits
 	.byte	1                       // Abbreviation Code
 	.byte	17                      // DW_TAG_compile_unit
@@ -943,7 +942,7 @@ main:
 	.word	0                       // End Mark
 .LpubTypes_end0:
 
-	.ident	"Android (4639204-cfp-jopp based on r316199) clang version 6.0.1 (https://android.googlesource.com/toolchain/clang 279c0d3a962121a6d1d535e7b0b5d9d36d3c829d) (https://android.googlesource.com/toolchain/llvm aadd87ffb6a2eafcb577913073d46b20195a9cdc) (based on LLVM 6.0.1svn)"
+	.ident	"Android (4639204 based on r316199) clang version 6.0.1 (https://android.googlesource.com/toolchain/clang 279c0d3a962121a6d1d535e7b0b5d9d36d3c829d) (https://android.googlesource.com/toolchain/llvm aadd87ffb6a2eafcb577913073d46b20195a9cdc) (based on LLVM 6.0.1svn)"
 	.section	".note.GNU-stack","",@progbits
 	.section	.debug_line,"",@progbits
 .Lline_table_start0:
